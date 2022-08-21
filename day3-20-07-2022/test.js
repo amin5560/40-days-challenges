b73a691920a8670748d0e1d773a21a61a4a14b4f
amin5560 <aminaminurrahman@gmail.com>
2022-08-21 13:24:01 +0600

    learn let keyword

---
+++ day3-20-07-2022/test.js
@@ -0,0 +1,23 @@
+/* let x = 7;
+if (true) {
+    let x = 6;
+    console.log(x);
+}
+console.log(x); */
+
+
+
+/* re declaring the block 
+
+var x = 10;
+{
+    var x = 2;
+}
+console.log(x); */
+
+/*
+let a; // declare
+a = 5;
+
+a = 7; // reassign
+*/
