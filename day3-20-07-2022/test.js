2c042df435266f32e36a03ebaef2e99d8a4f97d9
amin5560 <aminaminurrahman@gmail.com>
2022-08-21 16:15:15 +0600

    finished operators

--- day3-20-07-2022/test.js
+++ day3-20-07-2022/test.js
@@ -21,3 +21,19 @@ a = 5;
 
 a = 7; // reassign
 */
+
+
+
+/* let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z); */ // typeOf operator
+
+// What is the output of code below?
+let x = 29 % 9;
+console.log(x);
+
+let age = 25 + " years old";
+console.log(typeof age);
