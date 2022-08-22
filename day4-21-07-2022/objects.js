90d0aff36b11ed2cd74742ecbff5a8f4f1e078e5
amin5560 <aminaminurrahman@gmail.com>
2022-08-22 23:51:06 +0600

    complete object

--- day4-21-07-2022/objects.js
+++ day4-21-07-2022/objects.js
@@ -1,13 +1,30 @@
-const car = {
+/* const car = {
     name: "Flat",
     model: 500,
     weight: "850kg",
     color: "white",
     start: function () {
+        this.drive
         console.log("car has started");
     },
     drive: function () {
-        console.log("car is driving");
+        console.log("car is driving"); //object methods
     }
 };
 console.log(car.weight);
+console.log(car['model']);
+car.start();
+*/
+
+//let x = "Rahim"; //literal syntax
+
+// let x = new Number(5);
+// console.log(x);
+
+function sayHi() {
+    console.log(name);
+    console.log(age);
+    var name = 'JavaScript';
+    let age = 21;
+}
+sayHi()
