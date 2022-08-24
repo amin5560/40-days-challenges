f4f247b1897f8564680e667548eff522071e78fe
amin5560 <aminaminurrahman@gmail.com>
2022-08-24 20:14:10 +0600

    learning string replace

---
+++ day5-22-08-2022/string-methods.js
@@ -0,0 +1,20 @@
+/* let str = "Apple, Banana, Kiwi";
+console.log(str.slice(7, 13)); */
+
+let str = "Please visit Microsoft!";
+let newStr = str.replace("Microsoft", "W3School");
+console.log(newStr);
+
+let text1 = "Hello World!";
+let text2 = text1.toUpperCase();
+console.log(text2); //JavaScript String toUpperCase()
+
+let text3 = "HeLLo World!";       // String
+let text4 = text3.toLowerCase();
+console.log(text4); //JavaScript String toLowerCase()
+
+
+let text5 = "Hello";
+let text6 = "Zilan";
+let text7 = text5.concat(" ", text6);
+console.log(text7);
